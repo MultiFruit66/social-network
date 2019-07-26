8099969adad8b9e678e8d86bd23aafcada629674
Maxuta Pavel <MaxutaPavel@gmail.com>
2019-07-26 14:23:37 +0300

    Updates

---
+++ precache-manifest.52760c61fc8586d3a05a75702de02edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d694e2b0a6ee3082d226a5eb133d81",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "d47332ed2ec4fb685acd",
+    "url": "/social-network/static/css/main.750d269f.chunk.css"
+  },
+  {
+    "revision": "e3b69759771ab5d94e1e",
+    "url": "/social-network/static/js/2.389ef943.chunk.js"
+  },
+  {
+    "revision": "d47332ed2ec4fb685acd",
+    "url": "/social-network/static/js/main.f45a37d8.chunk.js"
+  },
+  {
+    "revision": "cd8926f0a4b82e4e81df",
+    "url": "/social-network/static/js/runtime~main.eb74f080.js"
+  }
+]);
